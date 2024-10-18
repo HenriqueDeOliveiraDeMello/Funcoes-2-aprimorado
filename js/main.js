254561dddd3651eb2138047e7be6081570efbade
Henrique de Oliveira de Mello <107197744+HenriqueDeOliveiraDeMello@users.noreply.github.com>
2024-10-18 10:49:03 -0300

    18/10

--- js/main.js
+++ js/main.js
@@ -88,8 +88,8 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
