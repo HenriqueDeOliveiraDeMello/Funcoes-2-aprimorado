9829983a5d87f3b2a5d16e45cf882db71f1ab871
Henrique de Oliveira de Mello <107197744+HenriqueDeOliveiraDeMello@users.noreply.github.com>
2024-10-25 10:40:26 -0300

    25/10

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-import {aleatorio} from "./aleatorio.js";
-import {perguntas} from "./perguntas.js";
-
+import { aleatorio } from "./aleatorio.js";
+import { perguntas } from "./perguntas.js";
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-
+const botaoJogarNovamente = document.querySelector(".novamente.btn")
 
 let atual = 0;
 let perguntaAtual;
@@ -45,6 +43,13 @@ function mostraResultado() {
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+    botaoJogarNovamente.addEventListener("click", jogarNovamente)
+}
+
+function jogarNovamente() {
+    atual = 0;
+    historiaFinal = "";
+    mostraPergunta();
 }
 
 mostraPergunta();
